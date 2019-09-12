7a050218565bef70c08a5f8c141fed683217386d
Dodo <kasper199914@gmail.com>
2019-09-12 14:17:26 +0200

    whishercode

--- whiskers.c
+++ whiskers.c
@@ -4,17 +4,33 @@
   Display whisker states in terminal.  1 = not pressed, 0 = pressed. 
 */
 
-#include "simpletools.h"                      // Include simpletools header
+#include "simpletools.h"
 
-int main()                                    // main function
+int main()
 {
   freqout(4, 2000, 3000);                     // Speaker tone: P4, 2 s, 3 kHz
-  while(1)                                    // Endless loop
-  {
-    int wL = input(7);                        // Left whisker -> wL variable
-    int wR = input(8);                        // Right whisker -> wR variable
-    print("%c", HOME);                        // Terminal cursor home (top-left)
-    print("wL = %d  wR = %d", wL, wR);        // Display whisker variables
-    pause(50);                                // Pause 50 ms before repeat
+  
+  for(;;) {    
+      // as long as there's no initial touch, continue
+      if (input(8)) { continue; }
+      
+      unsigned long int count = 0;
+      unsigned long int start_time = CNT;
+      int prv = 0;
+         
+      for(;;) {
+        int current = input(8);
+           
+        if(prv != current) {
+          count += 1;
+        }
+            
+        prv = current;
+            
+        if(CNT > (start_time + CLKFREQ * 5) && count > 0) {
+          print("Aantal metingen = %d\n", count);
+          break;
+        }
+      }
   }
 }
