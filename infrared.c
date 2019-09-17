2d7f994af7dd115a7baf4863d68b92a7ec0d17a5
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-09-17 16:31:15 +0200

    idk

--- infrared.c
+++ infrared.c
@@ -13,28 +13,44 @@ int main()
 
     if (irRight == 0) {
 
-      print("%c Started counter\n", HOME);
+      print("Started counter\n");
 
 
       unsigned long int count = 0;
       unsigned long int start_time = CNT;
+      
+      print("CPU time before count: %u\n", CNT);
+      
+      long int zeroes = 0;
+      long int ones = 0;
 
       for (;;) {
         freqout(1, 1, 38000);
         irRight = input(2);
         
+        if (irRight == 0) {
+          zeroes += 1;
+        } else {
+          ones += 1;
+        }          
+        
         count += 1;
 
-        if(CNT > (start_time + CLKFREQ * 5) && count > 0) {
+        if(CNT > (start_time + CLKFREQ * 1) && count > 0) {
           print("Aantal metingen in 5 sec = %d\n", count);
+                    print("Aantal 0 in 5 sec = %d \nAantal 1 in 5 sec = %d\n", zeroes, ones);
+
+          print("CPU time after count: %u\n", CNT);
+
           break;
         }
       }
 
     }
     
-    print("%c irRight = %d\n", HOME, irRight);
+    print("irRight = %d\n", irRight);
+   
     
-    pause(1000);
+    pause(3000);
   }
 }
