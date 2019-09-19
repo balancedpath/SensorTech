dfe1a4515fbc2036022475cc8920dc48f0090a3c
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-09-19 14:56:44 +0200

    added old cmm

---
+++ huisje.c
@@ -0,0 +1,13 @@
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+  // Add startup code here.
+
+ 
+  while(1)
+  {
+    // Add main loop code here.
+    
+  }  
+}
